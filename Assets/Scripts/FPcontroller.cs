4d7280f0393e07818d108f5f630a2e2045eb2a85
guilherme_hq <quintana.guilherme@gmail.com>
2021-08-30 22:59:36 -0300

    fix player camera/ add movement and jump

--- Assets/Scripts/FPcontroller.cs
+++ Assets/Scripts/FPcontroller.cs
@@ -7,13 +7,19 @@ public class FPcontroller : MonoBehaviour
     public Transform mainCamera;
     float angle = 0;
     public float cameraSpeedX = 10;
-    public float cameraSpeedY = -10; 
+    public float cameraSpeedY = -10;
+    public float playerSpeed = 3;
+    public float playerJump = 5;
+    Rigidbody playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
         // makes cursor disappear
         Cursor.lockState = CursorLockMode.Locked;
+
+        //access player Rigidbody
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -25,5 +31,10 @@ public class FPcontroller : MonoBehaviour
         //rotate camera in the Y angle
         angle = Mathf.Clamp(Input.GetAxis("Mouse Y") * cameraSpeedY + angle, -60, 60);
         mainCamera.localRotation = Quaternion.Euler(new Vector3(angle, 0, 0));
+
+        //Movement
+        Vector3 velocity = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * playerSpeed;
+        velocity.y = Input.GetKeyDown(KeyCode.Space) ? playerJump : playerRigidbody.velocity.y;
+        playerRigidbody.velocity = velocity;
     }
 }
