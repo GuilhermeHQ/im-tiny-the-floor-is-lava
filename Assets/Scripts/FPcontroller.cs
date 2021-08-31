3dd17ae75f873dcd7f67a806b5cb6f0f612a5581
guilherme_hq <quintana.guilherme@gmail.com>
2021-08-30 23:42:54 -0300

    start ground check

--- Assets/Scripts/FPcontroller.cs
+++ Assets/Scripts/FPcontroller.cs
@@ -6,10 +6,14 @@ public class FPcontroller : MonoBehaviour
 {
     public Transform mainCamera;
     float angle = 0;
+    public LayerMask layerMask;
+
     public float cameraSpeedX = 10;
     public float cameraSpeedY = -10;
     public float playerSpeed = 3;
     public float playerJump = 5;
+
+    RaycastHit hit;
     Rigidbody playerRigidbody;
 
     // Start is called before the first frame update
@@ -34,7 +38,9 @@ public class FPcontroller : MonoBehaviour
 
         //Movement
         Vector3 velocity = transform.rotation * new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * playerSpeed;
-        velocity.y = Input.GetKeyDown(KeyCode.Space) ? playerJump : playerRigidbody.velocity.y;
+        velocity.y = Input.GetKeyDown(KeyCode.Space) & Physics.SphereCast(transform.position, .23f, Vector3.down, out hit, .3f, layerMask) ? playerJump : playerRigidbody.velocity.y;
+        //& Physics.SphereCast(transform.position, .23f, Vector3.down, out hit, .3f, layerMask) 
+
         playerRigidbody.velocity = velocity;
     }
 }
